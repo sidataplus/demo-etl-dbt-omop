1864e76cbadd34fe29ce141202bac0a148de3882
Thanapat Pitchayarat <69158150+ThanePi@users.noreply.github.com>
2022-10-11 16:55:03 +0700

    more test and more model

--- lakehouse/postgres_indices.sql
+++ lakehouse/postgres_indices.sql
@@ -1,2 +1,8 @@
 /* Turn these columns into columnar for performance improve */
 create index idx_concept on cdm.concept (concept_code, vocabulary_id)
+
+create index idx_synthea_encounters on source.encounters (id, patient, organization, provider, payer);
+create index idx_synthea_medications on source.medications (patient, payer, encounter);
+create index idx_synthea_medications_code on source.medications using btree (code);
+create index idx_synthea_immunizations on source.immunizations (patient, encounter);
+create index idx_synthea_immunizations_code on source.immunizations using btree (code);
